735018ca94a1e30b15a1d1e3e49a9e2992a2d8b0
vagrant <vagrant@precise32.(none)>
2015-03-06 01:38:44 +0000

    testing margins

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@
  * GET home page.
  */
 
+
+
 exports.view = function(req, res){
-	var random_num = Math.random();
-	if(random_num > 0.5) {
+	
 	res.render("index", {'error': false});
-	}
-	else {
-	res.render("index2", {'error': false});
-	}
+
 };
